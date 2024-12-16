eb9c32514ca358c5d15e63b2bd279a0bf9c975b8
Muhammad Hamza <98486587+Hamzakabeer@users.noreply.github.com>
2024-12-16 18:55:08 +0500

    Initial commit

---
+++ javascriptass.js
@@ -0,0 +1,81 @@
+// Chapter no 1
+// Print User Information
+alert("Error! Please enter a valid password.");
+
+// print the welcome message
+alert("Wlecome to js Land...\nHappy coding");
+
+// print the message console
+console.log("Hello... I can run Js through my web browser's console");
+
+// End
+
+// Chapter no 2
+// variables for Strings
+alert("Muhammad Hamza Kabeer");
+
+alert("22 years old");
+
+alert("Certified Mobile Application Development");
+
+// Js Variables
+alert("PIZZA\nPIZZ\nPIZ\nPI\nP");
+
+// Alert Email Address...
+alert("My Email Address is hamzakabeer021@gmail.com");
+
+// Alert message box
+alert("I am learning for javascript");
+
+// Message on Browser screen
+document.write("Yah! I can write HTML content through JavaScript");
+
+// Store the string in a variable
+var myString = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+        
+// Show the string in an alert box
+alert(myString);
+
+// Display the string in the browser
+document.write("<h2>" + myString + "</h2>");
+
+// End
+
+// Chapter no 3
+// variable for numbers
+var age = 22; // 
+alert("I am " + age + " years old");
+
+// Check if a visit count exists in local storage
+var visitCount = localStorage.getItem("visitCount");
+
+// Initialize or increment the visit count
+if (visitCount === null) {
+    visitCount = 1;
+} else {
+    visitCount = parseInt(visitCount) + 1; 
+}
+
+// Save the updated visit count to local storage
+localStorage.setItem("visitCount", visitCount);
+
+// Display the visit count in an alert box
+alert("You have visited this site " + visitCount + " times");
+
+ // Declare a variable and assign your birth year
+ var birthYear = 1990;
+
+ // Display the messages in the browser
+ document.write("My birth year is " + birthYear + "<br>");
+ document.write("Data type of my declared variable is " + typeof birthYear + "<br></br>");
+
+ // Variables to store visitor's information
+let visitorName = "Muhammad Hamza Kabeer"; 
+let productTitle = "HaksShop.com"; 
+let quantity = 10; 
+
+// Message to display in the browser
+let message = `<strong>${visitorName}</strong> ordered <strong>${quantity} ${productTitle}(s)</strong> on XYZ Clothing store`;
+
+// Display the message on the browser
+document.write(message);
