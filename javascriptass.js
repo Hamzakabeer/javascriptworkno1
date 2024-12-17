43ff6bb27f00ffc1ba3ddda211fcb43d697b84da
Muhammad Hamza <98486587+Hamzakabeer@users.noreply.github.com>
2024-12-17 13:03:45 +0500

    update random q no add

--- javascriptass.js
+++ javascriptass.js
@@ -1,54 +1,72 @@
 // // Chapter no 1
 // // Print User Information
+// Q no 1
 // alert("Error! Please enter a valid password.");
-
+// Q no 2
 // // print the welcome message
 // alert("Wlecome to js Land...\nHappy coding");
-
+// Q no 3
 // // print the message console
 // console.log("Hello... I can run Js through my web browser's console");
 
 // // End
 
 // // Chapter no 2
+// Q no 1
 // // variables for Strings
 // alert("Muhammad Hamza Kabeer");
 
+// Q no 2
+// Age Define.
 // alert("22 years old");
 
-// alert("Certified Mobile Application Development");
+// Q no 3
+// Who is Become a ?
+// alert("One Day Ishallah Will be a good developer");
 
+// Q no4
 // // Js Variables
 // alert("PIZZA\nPIZZ\nPIZ\nPI\nP");
 
+
+// Q no5
 // // Alert Email Address...
 // alert("My Email Address is hamzakabeer021@gmail.com");
 
+// Q no6
 // // Alert message box
 // alert("I am learning for javascript");
 
+
+// Q no7
 // // Message on Browser screen
 // document.write("Yah! I can write HTML content through JavaScript");
 
+// Q no8
 // // Store the string in a variable
 // var myString = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
-        
+  
+// Q no9
 // // Show the string in an alert box
 // alert(myString);
 
+// Q no10
 // // Display the string in the browser
 // document.write("<h2>" + myString + "</h2>");
 
 // // End
 
 // // Chapter no 3
+
+// Q no1
 // // variable for numbers
 // var age = 22; // 
 // alert("I am " + age + " years old");
 
+
+// Q no2
 // // Check if a visit count exists in local storage
 // var visitCount = localStorage.getItem("visitCount");
-
 // // Initialize or increment the visit count
 // if (visitCount === null) {
 //     visitCount = 1;
@@ -62,6 +80,7 @@
 // // Display the visit count in an alert box
 // alert("You have visited this site " + visitCount + " times");
 
+// Q no3
 //  // Declare a variable and assign your birth year
 //  var birthYear = 1990;
 
@@ -69,6 +88,7 @@
 //  document.write("My birth year is " + birthYear + "<br>");
 //  document.write("Data type of my declared variable is " + typeof birthYear + "<br></br>");
 
+// Q no4
 //  // Variables to store visitor's information
 // let visitorName = "Muhammad Hamza Kabeer"; 
 // let productTitle = "HaksShop.com"; 
